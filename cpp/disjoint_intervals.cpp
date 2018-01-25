1a64b04207adbd261bc4032b635444281ebd1261
inxell <jinxwell@gmail.com>
2018-01-24 23:12:19 -0500

    comments added

--- cpp/disjoint_intervals.cpp
+++ cpp/disjoint_intervals.cpp
@@ -4,6 +4,7 @@ typedef set<pair<int, int>>::iterator iiter;
 class Intervals {
 public:
     void add(const pair<int, int> & p){
+        /// add interval p into member pset and merge overlapping intervals.
         if(p.first > p.second){
             throw runtime_error("Invalid Interval!");
         }
@@ -21,6 +22,8 @@ public:
     }
 
     vector<pair<int, int>> intersect(const pair<int, int>& p) const{
+        /// return intersection of p with intervals in pset.
+        /// only intervals with posive width are returned.
         if(p.first > p.second){
             throw runtime_error("Invalid Interval!");
         }
@@ -40,6 +43,10 @@ public:
 private:
     set<pair<int, int>> pset;
     pair<iiter, iiter> equalrange(const pair<int, int>& p) const{
+        ''' 
+        return a pair of iterators of pset,  (lit, rit), such that for any it within 
+        [lit, rit), the interval *it intersets with p.
+        '''
         auto lit = pset.lower_bound({p.first, p.first});
         auto rit = pset.lower_bound({p.second, p.second});
         if(lit != pset.begin() && (--lit)->second < p.first){
